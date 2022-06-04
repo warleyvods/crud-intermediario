5c9d5b0e3ba2f4a2b318cecfa08217bb6cd0521d
Warley <warleyvods@gmail.com>
2022-06-04 14:53:52 -0300

    add features

--- src/main/java/com/example/crudintermediario/controllers/AddressController.java
+++ src/main/java/com/example/crudintermediario/controllers/AddressController.java
@@ -1,6 +1,8 @@
 package com.example.crudintermediario.controllers;
 
 import com.example.crudintermediario.controllers.dtos.requests.AddressRequestDTO;
+import com.example.crudintermediario.controllers.dtos.responses.AddressResponseDTO;
+import com.example.crudintermediario.controllers.mappers.AddressMapper;
 import com.example.crudintermediario.gateways.AddressGateway;
 import com.example.crudintermediario.models.Address;
 import org.springframework.http.HttpStatus;
@@ -13,23 +15,24 @@ import java.util.List;
 public class AddressController {
 
     private final AddressGateway addressGateway;
+    private final AddressMapper addressMapper;
 
-    public AddressController(AddressGateway addressGateway) {
+    public AddressController(AddressGateway addressGateway, AddressMapper addressMapper) {
         this.addressGateway = addressGateway;
+        this.addressMapper = addressMapper;
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public Address save(@RequestBody AddressRequestDTO addressRequestDTO) {
-
-
-        return null;
+    public AddressResponseDTO save(@RequestBody AddressRequestDTO addressRequestDTO) {
+        Address address = addressMapper.toModel(addressRequestDTO);
+        return addressMapper.fromModel(addressGateway.save(address));
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
-    public List<Address> listAll() {
-        return addressGateway.listAll();
+    public List<AddressResponseDTO> listAll() {
+        return addressMapper.fromModelList(addressGateway.listAll());
     }
 
 }
